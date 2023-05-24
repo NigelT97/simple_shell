9c2eda3eb642e6ce69d8d0415a8438daf99eae53
calvinpatriot <calvinpatriot@gmail.com>
2023-05-24 22:39:53 +0200

    shell task

--- shell.h
+++ shell.h
@@ -1,6 +1,12 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+*
+* File: shell.h
+* Auth: Calvin Kudiwapfava
+*       Nigel Masiyazi-Ngorima
+*/
+
 #include <fcntl.h>
 #include <signal.h>
 #include <sys/types.h>
@@ -83,5 +89,42 @@ char *error_2_syntax(char **args);
 
 int hist;
 
+/* Str functions */
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strncat(char *dest, const char *src, size_t n);
+char *_strcpy(char *dest, const char *src);
+char *_strchr(char *s, char c);
+int _strlen(const char *s);
+char *_strcat(char *dest, const char *src);
+int _strspn(char *s, char *accept);
 
+
+/* Builtins 
+int (*get_builtin(char *command))(char **args, char **front);
+int shellby_exit(char **args, char **front);
+int shellby_env(char **args, char __attribute__((__unused__)) **front);
+int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_cd(char **args, char __attribute__((__unused__)) **front);
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+int shellby_help(char **args, char __attribute__((__unused__)) **front); */
+
+/* Linkedlist */
+t_alias *add_alias_end(t_alias **head, char *name, char *value);
+void free_alias_list(t_alias *head);
+t_list *add_node_end(t_list **head, char *dir);
+void free_list(t_list *head);
+
+void help_unsetenv(void);
+void help_history(void);
+void help_help(void);
+void help_all(void);
+void help_alias(void);
+void help_env(void);
+void help_setenv(void);
+void help_cd(void);
+void help_exit(void);
+
+int proc_file_commands(char *file_path, int *exe_ret);
 #endif
